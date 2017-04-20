4320ff89665a2da0c17e3d6716b4dca7cf905dbf
Robert Jankowski <jankowski.r@hotmail.com>
2017-04-20 15:04:50 +0200

    dodanie complex command

---
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -0,0 +1,42 @@
+package edu.iis.powp.command;
+
+import java.util.List;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class ComplexCommand implements PlotterCommand {
+
+	private IPlotter plotterDriver;
+	List<PlotterCommand> commands;
+
+	public ComplexCommand(IPlotter plotterDriver) {
+		super();
+		this.plotterDriver = plotterDriver;
+	}
+
+	@Override
+	public void execute() {
+		for (PlotterCommand plotterCommand : commands) {
+			plotterCommand.setDriver(plotterDriver);
+			plotterCommand.execute();
+		}
+	}
+
+	public void addCommand(PlotterCommand command) {
+		commands.add(command);
+	}
+
+	public void removeCommand(PlotterCommand command) {
+		commands.remove(command);
+	}
+
+	public PlotterCommand getCommand(int position) {
+		return commands.get(position);
+	}
+
+	@Override
+	public void setDriver(IPlotter plotter) {
+		this.plotterDriver = plotter;
+	}
+
+}
